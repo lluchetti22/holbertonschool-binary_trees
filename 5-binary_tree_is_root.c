8be8bc5d265274f8c8e6541f9b3853cf960935b0
Lachlan <lachlan.luchetti@outlook.com>
2026-04-07 16:06:57 +1000

    5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_leaf - Binary tree node
+ * @node: node
+ * Return: the code
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node == NULL)
+{
+return (0);
+}
+
+if (node->parent == NULL)
+{
+return (1);
+}
+
+return (0);
+}
