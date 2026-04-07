5632b48b4f5418b638e2184d06a24ee835104259
Lachlan <lachlan.luchetti@outlook.com>
2026-04-07 17:14:42 +1000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 
 /**
+ * tree_height - Binary tree node
+ * @tree: node
+ * Return: the code
+ */
+
+size_t tree_height(const binary_tree_t *tree)
+{
+
+size_t left_h = 0;
+size_t right_h = 0;
+
+if (tree == NULL)
+{
+return (0);
+}
+
+left_h = tree_height(tree->left);
+right_h = tree_height(tree->right);
+
+if (left_h > right_h)
+{
+return (left_h + 1);
+}
+
+return (right_h + 1);
+}
+
+
+/**
  * binary_tree_nodes - Binary tree node
  * @tree: node
  * Return: the code
