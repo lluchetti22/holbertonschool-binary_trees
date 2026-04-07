17ee783388e06c580ef0fbc0ca379075643874a5
Lachlan <lachlan.luchetti@outlook.com>
2026-04-07 16:47:15 +1000

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_height - Binary tree node
+ * @tree: node
+ * Return: the code
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t depth = 0;
+
+
+if (tree == NULL)
+{
+return (0);
+}
+
+while (tree->parent != NULL)
+{
+depth++;
+tree = tree->parent;
+}
+
+return (depth);
+}
