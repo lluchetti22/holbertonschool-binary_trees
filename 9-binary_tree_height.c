0c1da030b6d1b6b3099e17b4d59e58557a2966d6
Lachlan <lachlan.luchetti@outlook.com>
2026-04-07 16:34:34 +1000

    9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_postorder - Binary tree node
+ * @tree: node
+ * Return: the code
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left_h = 0;
+size_t right_h = 0;
+
+if (tree == NULL)
+{
+return;
+}
+
+left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+return (left_h > right_h ? left_h : right_h);
+}
